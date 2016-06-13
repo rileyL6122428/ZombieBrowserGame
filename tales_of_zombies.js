0dc5daf3e28c5a86c59031a55c5b317628f9a74a
Riley Littlefield <rileylittlefield@ymail.com>
2016-06-13 00:14:17 -0400

    adds basic environment

--- tales_of_zombies.js
+++ tales_of_zombies.js
@@ -2,12 +2,12 @@ var GameView = require('./lib/game_view');
 
 
 var canvasEl = document.getElementById("game-canvas");
-      // canvasEl.height = window.innerHeight;
-      // canvasEl.width = window.innerWidth;
-      console.log(canvasEl.height);
+
       var ctx = canvasEl.getContext('2d');
-      ctx.fillStyle = "red";
       ctx.fillRect(20,30,40,50);
+      ctx.canvas.height = 750;
+      ctx.canvas.width  = 1300;
+      ctx.fillStyle = "red";
 
 var gameView = new GameView(ctx, canvasEl.width, canvasEl.height);
       gameView.start();
