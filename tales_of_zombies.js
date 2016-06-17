4800e3828380b578d3819b8cc8e94ac00fd959e6
Riley Littlefield <rileylittlefield@ymail.com>
2016-06-17 16:48:26 -0400

    adds instructions to title screen

--- tales_of_zombies.js
+++ tales_of_zombies.js
@@ -2,12 +2,9 @@ var GameView = require('./lib/game_view');
 
 
 var canvasEl = document.getElementById("game-canvas");
-
-      var ctx = canvasEl.getContext('2d');
-      ctx.fillRect(20,30,40,50);
-      ctx.canvas.height = 750;
-      ctx.canvas.width  = 1300;
-      ctx.fillStyle = "red";
+var ctx = canvasEl.getContext('2d');
+ctx.canvas.height = 750;
+ctx.canvas.width  = 1300;
 
 var gameView = new GameView(ctx, canvasEl.width, canvasEl.height);
-      gameView.start();
+gameView.start();
